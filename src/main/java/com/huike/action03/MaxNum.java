4da72c461273b0e7ef0a0e14518de6c67186b6a2
evoup <evoex123@gmail.com>
2016-12-01 17:53:45 +0800

    add log for action03

--- src/main/java/com/huike/action03/MaxNum.java
+++ src/main/java/com/huike/action03/MaxNum.java
@@ -2,6 +2,9 @@ package com.huike.action03;
 
 import java.io.IOException;
 
+import com.google.gson.Gson;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
@@ -17,13 +20,17 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class MaxNum extends Configured implements Tool {
+	private static final Log LOG = LogFactory.getLog(MaxNum.class);
 
 	public static class MaxNumMapper extends Mapper<LongWritable, Text, Text, Text> {
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
+			LOG.info("[MaxNumMapper][line:" + line + "]");
 			String[] parameters = line.split("\\s+");
+			LOG.info("[MaxNumMapper][parameters:" + new Gson().toJson(parameters) + "]");
 			String deptid = parameters[0];
+			LOG.info("[MaxNumMapper][deptid:" + deptid + "]");
 			context.write(new Text(deptid), new Text(String.valueOf(parameters[1])));
 		}
 	}
@@ -31,11 +38,15 @@ public class MaxNum extends Configured implements Tool {
 	public static class MaxNumReducer extends Reducer<Text, Text, Text, Text> {
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 			long max = 0L;
+			LOG.info("[MaxNumReducer]");
 			for (Text text : values) {
+				LOG.info("[MaxNumReducer][values][" + new Gson().toJson(values) +"]");
 				if (Long.parseLong(text.toString()) > max) {
 					max = Long.parseLong(text.toString());
+					LOG.info("[MaxNumReducer][max:" + max + "]");
 				}
 			}
+			LOG.info("[MaxNumReducer][key:" + key + "][value:" + new Text(String.valueOf(max)) + "]");
 			context.write(key, new Text(String.valueOf(max)));
 		}
 	}
